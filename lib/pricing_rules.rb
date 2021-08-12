a1f543880c7c1b71b729900211ca32e019a53f1e
Ivan Alvarez <38564316+Vaniik@users.noreply.github.com>
2021-08-12 22:17:52 +0200

    added the given rules

--- lib/pricing_rules.rb
+++ lib/pricing_rules.rb
@@ -18,4 +18,19 @@ class PricingRules
   def delete_product(product_code)
     @product_list.delete(product_code)
   end
+
+  def get_price(product, amount)
+    product_price = product_list[product].price
+    price = case product
+            when 'GR1'
+              amount.even? == true ? product_price * amount / 2 : product_price * ((amount - 1) / 2 + 1)
+            when 'SR1'
+              amount >= 3 ? (product_price - 0.5) * amount : product_price * amount
+            when 'CF1'
+              amount >= 3 ? (product_price * 2 / 3) * amount : product_price * amount
+            else
+              product_price * amount
+            end
+    price.round(2)
+  end
 end
